a8dc747e34a70cb2ca705557a5f5aa96ccd9804d
cmrsl <121862347+cmrsl@users.noreply.github.com>
2023-04-17 11:16:01 +0530

    Push Notification

--- src/views/pushNotification/index.js
+++ src/views/pushNotification/index.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { Card, CardHeader, CardTitle, CardBody, Row, Col } from 'reactstrap'
 import Form from './Form'
 import NotificationHub from '../../@core/layouts/components/navbar/NotificationHub'
+import Test from './Test'
+import DripCampaign from './dripCampaign/DripCampaign'
 
 const ChartContainer1 = () => {
 
@@ -10,8 +12,10 @@ const ChartContainer1 = () => {
             <Row className="match-height">
                 <Col xs="12">
                     <div className='d-flex row justify-content-around'>
-                        <Form />
+                        {/* <Form /> */}
                         {/* <NotificationHub /> */}
+                        {/* <Test /> */}
+                        <DripCampaign />
                     </div>
                 </Col>
             </Row>
