750ea4fcf5f6828db50817227a171779d4ab712c
Sahil Nagpal <31477461+Nagpalji@users.noreply.github.com>
2023-06-07 10:54:31 +0530

    firebase update with click parameter

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -16,8 +16,8 @@ var firebaseConfig = {
   storageBucket: "cmgalaxy-8de7d.appspot.com",
   messagingSenderId: "595038576433",
   appId: "1:595038576433:web:73e296d27fbda0f44b9a10",
-  measurementId: "G-GKSE4F21K7"
-};;
+  measurementId: "G-GKSE4F21K7",
+};
 
 firebase.initializeApp(firebaseConfig);
 
@@ -33,13 +33,17 @@ messaging.onBackgroundMessage(function (payload) {
     body: payload.notification.body,
   };
   var action_click = payload.data.click_action;
-  console.log('====================================');
-  console.log({action_click});
-  console.log('====================================');
+  console.log("====================================");
+  console.log({ action_click });
+  console.log("====================================");
   // eentpayload.openWindow(action_click);
   self.registration.showNotification(notificationTitle, notificationOptions);
   self.addEventListener("notificationclick", (event) => {
     event.notification.close();
+    firebase.analytics().logEvent("notificationclick", {
+      campaignId: payload.data.campaignId,
+      notificationId: payload.data.notificationId,
+    });
     event.waitUntil(clients.openWindow(action_click));
   });
 });
